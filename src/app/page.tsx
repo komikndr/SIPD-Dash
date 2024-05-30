46dff425afaeaf0cd6c9cc39affa526fc0290cdb
Micko <mickolesmana@gmail.com>
2024-05-30 11:36:21 +0700

    Using template from ShadCN Blocks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,66 @@
-import { Button } from "@/components/ui/button";
+
+import { Button } from "@/components/ui/button"
+import {
+  Bell,
+  CircleUser,
+  LineChart,
+  Package,
+  Package2,
+  Search,
+  ShoppingCart,
+  Users,
+} from "lucide-react"
+
+import { Badge } from "@/components/ui/badge"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { Input } from "@/components/ui/input"
+import SideNav from "@/components/ui/sideNav"
+import Header from "@/components/ui/header"
 
 export default function Home() {
   return (
-    <div className="budi">
-      <Button>Budi</Button>
+    <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+      <div className="hidden border-r bg-muted/40 md:block">
+      <SideNav />
+      </div>
+
+      <div className="flex flex-col">
+        <Header />
+
+
+        <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
+          <div className="flex items-center">
+            <h1 className="text-lg font-semibold md:text-2xl">Inventory</h1>
+          </div>
+          <div
+            className="flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm" x-chunk="dashboard-02-chunk-1"
+          >
+            <div className="flex flex-col items-center gap-1 text-center">
+              <h3 className="text-2xl font-bold tracking-tight">
+                You have no products
+              </h3>
+              <p className="text-sm text-muted-foreground">
+                You can start selling as soon as you add a product.
+              </p>
+              <Button className="mt-4">Add Product</Button>
+            </div>
+          </div>
+        </main>
+      </div>
     </div>
-  );
+  )
 }
