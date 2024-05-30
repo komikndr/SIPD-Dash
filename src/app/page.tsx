ea0ff82e19818a380fc8b057263ab86dd7590a94
Micko <mickolesmana@gmail.com>
2024-05-30 17:13:29 +0700

    Adding OpenSearch API

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 import SideNav from "@/components/ui/sideNav"
 import Header from "@/components/ui/header"
 import Main from "@/components/ui/main"
+
+import fetchDataFromOpenSearch from "@/lib/opensearch"
 export default function Home() {
+    let k = fetchDataFromOpenSearch();  
   return (
     <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
       <div className="hidden border-r bg-muted/40 md:block">
